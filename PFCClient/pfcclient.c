2d2525567596f4c779b95b45a92fbb3fd7484b03
Brord <brordvanwierst@msn.com>
2013-11-19 10:42:36 +0100

    Created projects for client and server

---
+++ PFCClient/pfcclient.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   pfcclient.c
+ * Author: Brord
+ *
+ * Created on November 19, 2013, 10:37 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ PFCServer/pfcserver.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   pfcserver.c
+ * Author: Brord
+ *
+ * Created on November 19, 2013, 10:37 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
