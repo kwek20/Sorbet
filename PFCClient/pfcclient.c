0caf67b6b98e6b892440ce3d898542baba299254
bartjan <bartjan@localhost>
2013-11-20 12:41:42 +0100

    Eerste Veranderingen in de pfcclient + headerfile toegevoegd.

--- PFCClient/pfcclient.c
+++ PFCClient/pfcclient.c
@@ -1,20 +1,71 @@
 /* 
  * File:   pfcclient.c
- * Author: Brord
+ * Author: Bartjan Zondag & Kevin Rosendaal
  *
  * Created on November 19, 2013, 10:37 AM
  * Changed on November 19, 2013, 12:36 AM
  * Changed on November 19, 2013, 12:40 AM
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-
 /*
- * 
+ * Deze client kan een file uploaden naar de PFC server
  */
+
+#include "pfcclient.h"
+
 int main(int argc, char** argv) {
 
+    //pfcclient /tmp/test.txt 192.168.1.1
+    argc = 3;
+    argv[1] = "/tmp/test.txt";
+    argv[2] = "192.168.1.1"; //moet nog veranderd worden
+    
+    pfcClient(argv);
+    
+    /*
+     * invoer bestand
+     * invoer ip server
+     */
     return (EXIT_SUCCESS);
 }
 
+int pfcClient(char** argv){
+
+   struct sockaddr_in serv_addr;
+   char buffer[BUFFERGROOTE];
+
+
+   //memsets
+   memset(&serv_addr, '\0', sizeof (serv_addr));
+
+
+   /*
+    * Vul juiste gegevens in voor server in struct serv_addr
+    */
+   serv_addr.sin_family = AF_INET;
+
+   if (inet_pton(AF_INET, argv[2], &serv_addr.sin_addr) <= 0) {
+        perror("inet_pton error occured:");
+        exit(2);
+   }
+
+   serv_addr.sin_port = 2200;
+
+}
+/*
+ * Functie argv omzetten naar ip
+ */
+
+/*
+ * Functie controleerd of bestand bestaat
+ */
+
+/*
+ * Functie maak verbinding met de server
+ *
+ *
+ */
+
+/*
+ * Functie geef aan dat je een bestand wilt uploaden
+ */
