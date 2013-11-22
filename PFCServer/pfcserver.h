38aa713e5c63977953c228203138620db0634ecb
davevhooren <davybach@gmail.com>
2013-11-22 16:59:30 +0100

    Begin of command codes library

--- PFCServer/pfcserver.h
+++ PFCServer/pfcserver.h
@@ -19,3 +19,17 @@
 
 #define PORT 2200
 #define MAX_CLI 10
+
+// 1xx OK en gerelateerde statussen
+#define OK  100 // OK (bevestigingscode)
+#define EOF 101 // End-of-file
+// 2xx Errors en gerelateerde statussen
+#define CL  200 // Verbinding verbroken
+#define FC  201 // Bestand corrupt
+#define WPN 202 // Verkeerde packet nummer
+#define CNA 203 // Verbinding niet toegestaan
+// 3xx Client naar server requests
+#define CR  300 // Client vraagt bestandsoverdracht aan
+#define CC  301 // Client vraagt aan de server of er nieuwe/gewijzigde bestanden zijn.
+// 4xx Server naar client requests
+// ..
