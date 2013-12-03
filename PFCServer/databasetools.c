fb43306e69495adfa9881646966fefc03704da03
davevhooren <davybach@gmail.com>
2013-12-03 02:16:45 +0100

    Added databasetools functions and method pseudocode.

--- PFCServer/databasetools.c
+++ PFCServer/databasetools.c
@@ -6,17 +6,55 @@
  * Deze file zal later worden samengevoegd met de utils.c file.
  */
 
+#include <stdio.h>
+#include <sqlite3.h>
+#include <string.h>
+
 /*
  * Functies die in zowel de client- als serverapplicatie gelijk zijn.
  */
 
 #include "pfc.h"
 
-int connectDB();
-int updateDB();
-int closeDB();
+// ADDED PSEUDOCODE FOR LATER EDITING.
 
-int hashPassword();
+int main()
+{
+    sqlite3 *db;
+    char *ErrMsg = 0;
+    int rec;
+    
+    char *password;
+    
+    connectDB(rec);
+    //......
+    closeDB(db);
+}
+
+int connectDB(int rec, sqlite3 *db)
+{
+    rec = sqlite3_open(&db);
+}
+
+int updateDB(int rec)
+{
+    rec = sqlite3_exec();
+}
+
+int selectDB(int rec)
+{
+    
+}
+
+int closeDB(sqlite3 *db)
+{
+    sqlite3_close(db);
+}
+
+int hashPassword(char *password)
+{
+    //Crypto
+}
 
 // De onderstaande functies worden later verplaast
 
@@ -24,13 +62,31 @@ int hashPassword();
  * Functies die voor de serverapplicatie bedoeld zijn.
  */
 
-int createUser();
-int removeUser();
-int checkCredentials();
+int createUser(int rec)
+{
+    updateDB(rec);
+}
+
+int removeUser(int rec)
+{
+    updateDB(rec);
+}
+
+int checkCredentials()
+{
+    // Password == selectDB();
+}
 
 /*
  * Functies die voor de clientapplicatie bedoeld zijn.
  */
 
-int sendCredentials();
-int writePasswordToLocalDB();
+int sendCredentials()
+{
+    
+}
+
+int writePasswordToLocalDB(int rec)
+{
+    updateDB(rec);
+}
