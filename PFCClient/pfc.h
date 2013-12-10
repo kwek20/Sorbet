c52cafe2088420af93485bb09bb4185217a77f3c
kevin <kevin@rosendaal.net>
2013-12-10 13:45:03 +0100

    Poging tot mergen. #1

--- PFCClient/pfc.h
+++ PFCClient/pfc.h
@@ -26,6 +26,11 @@
 #include <sqlite3.h>
 #include <openssl/sha.h>
 
+#include <malloc.h>
+#include <resolv.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
 #define BUFFERSIZE 4096
 #define NETWERKPOORT 2200
 
@@ -33,6 +38,8 @@
 #define MOOI 0
 
 #define MAX_CLI 10
+
+#define CERTIFICATE "mycert.pem"
 
 // 1xx OK en gerelateerde statussen
 #define STATUS_OK  100 // OK (bevestigingscode)
@@ -67,18 +74,18 @@ int pfcClient(char** argv);
 int ServerGegevens(char* ip);
 int BestaatDeFile(char* fileName);
 int ConnectNaarServer(int* sockfd);
-int FileTransferSend(int* sockfd, char* bestandsnaam);
-int FileTransferReceive(int* sockfd, char* bestandsnaam, int time);
+int FileTransferSend(SSL *ssl, char* bestandsnaam);
+int FileTransferReceive(SSL *ssl, char* bestandsnaam, int time);
 int OpenBestand(char* bestandsnaam);
-int ModifyCheckServer(int* sockfd, char* bestandsnaam, char* timeleft);
-int ModifyCheckClient(int* sockfd, char* bestandsnaam);
+int ModifyCheckServer(SSL *ssl, char* bestandsnaam, char* timeleft);
+int ModifyCheckClient(SSL *ssl, char* bestandsnaam);
 
 int transform(char *text, char** to);
 int transformWith(char *text, char** to, char *delimit);
 
-int switchResult(int* sockfd, char* buffer);
-int sendPacket(int fd, int packet, ...);
-int waitForOk(int sockfd);
+int switchResult(SSL *ssl, char *buffer);
+int sendPacket(SSL *ssl, int packet, ...);
+int waitForOk(SSL *ssl);
 
 int changeModTime(char *bestandsnaam, int time);
 int modifiedTime(char *bestandsnaam);
@@ -93,7 +100,7 @@ int hashPassword(char *password, char *salt, char to[]);
 int randomSalt(char *salt, int aantalBytes);
 int convertHashToString(char *stringHash, unsigned char hash[]);
 
-int ConnectRefused(int* sockfd);
+int ConnectRefused(SSL *ssl);
 
 //DB - Will be edited
 int callback(void *NotUsed, int argc, char **argv, char **azColName);
@@ -115,3 +122,9 @@ int sendCredentials();
 int writePasswordToLocalDB(int rc);
 
 char* getSalt(char *name);
+
+int receiveSSL(SSL *ssl, char *buffer);
+
+SSL_CTX* InitServerCTX(void);
+void LoadCertificates(SSL_CTX* ctx, char* CertFile, char* KeyFile);
+void ShowCerts(SSL* ssl);
--- PFCServer/pfcserver.c
+++ PFCServer/pfcserver.c
@@ -16,7 +16,7 @@ void setupSIG();
 void SIGexit(int sig);
 
 void quit();
-void stopClient(int fd);
+void stopClient(SSL *ssl, SSL_CTX *ctx, int fd);
 
 void create(int *sock);
 struct sockaddr_in getServerAddr(int poort);
@@ -142,6 +142,15 @@ void create(int *sock){
     char buffer[BUFFERSIZE];
     char** to;
     
+    SSL_CTX *ctx;
+    SSL *ssl;
+
+    SSL_library_init();
+
+    // Init and load SSL
+    ctx = InitServerCTX();        /* initialize SSL */
+    LoadCertificates(ctx, CERTIFICATE, CERTIFICATE); /* load certs */
+    
     //open sem for new thread
     sem_post(&client_wait);
     
@@ -156,6 +165,19 @@ void create(int *sock){
     ip = inet_ntoa(client_addr.sin_addr);
     int poort = htons(client_addr.sin_port);
 
+	    //  SSL connect
+        //int client = accept(server, (struct sockaddr*)&addr, &len);  /* accept connection as usual */
+        //printf("Connection: %s:%d\n",inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+    ssl = SSL_new(ctx);              /* get new SSL state with context */
+    SSL_set_fd(ssl, fd);      /* set connection socket to SSL state */
+    
+    if ( SSL_accept(ssl) == STUK )     /* do SSL-protocol accept */
+    {
+        ERR_print_errors_fp(stderr);
+    }
+    
+    // Servlet(ssl);         /* service connection */
+
     //add to the list
     clients[fd-4].client = client_addr;
     
@@ -168,13 +190,13 @@ void create(int *sock){
     //Login moet nog naar een functie
     for (i = 0; i < LOGINATTEMPTS; i++){
         bzero(buffer, BUFFERSIZE);
-        if((to = malloc(recv(fd, buffer, BUFFERSIZE, 0))) < 0){
-            perror("recv error");
+        if((to = malloc(receiveSSL(ssl, BUFFERSIZE))) < 0){
+            perror("receive error");
             return;
         }
         transform(buffer, to);
         if((temp = ReceiveCredentials(to[1], to[2])) == MOOI){
-            sendPacket(fd, STATUS_AUTHOK, NULL);
+            sendPacket(ssl, STATUS_AUTHOK, NULL);
             //add username
             clients[fd-4].username = malloc(strlen(to[1]));
             strcpy(clients[fd-4].username, to[1]);
@@ -188,10 +210,10 @@ void create(int *sock){
         }
         
         if(i < 2){
-            sendPacket(fd, STATUS_AUTHFAIL, NULL);
+            sendPacket(ssl, STATUS_AUTHFAIL, NULL);
         } else {
-            sendPacket(fd, STATUS_CNA, NULL);
-            stopClient(fd);
+            sendPacket(ssl, STATUS_CNA, NULL);
+            stopClient(ssl, ctx, fd);
             return;
         }
     }
@@ -203,8 +225,8 @@ void create(int *sock){
     //loop forever until client stops
     for ( ;; ){
         //receive info
-        if ((rec = recv(fd, buffer, BUFFERSIZE,0)) < 0){
-            perror("recv");
+        if ((rec = receiveSSL(ssl, BUFFERSIZE)) < 0){
+            perror("receive");
             printf("Client error! Stopping... \n");
             break;
         } else if (rec == 0){
@@ -213,7 +235,7 @@ void create(int *sock){
             break;
         } else {
             //good
-            if ((result = switchResult(&fd, buffer)) == STUK){
+            if ((result = switchResult(ssl, buffer)) == STUK){
                 //error
                 break;
             } else {
@@ -222,7 +244,7 @@ void create(int *sock){
         }
         bzero(buffer, BUFFERSIZE);
     }
-    stopClient(fd);
+    stopClient(ssl, ctx, fd);
 }
 
 /**
@@ -249,7 +271,11 @@ void quit(){
     exit(MOOI);
 }
 
-void stopClient(int fd){
+void stopClient(SSL *ssl, SSL_CTX *ctx, int fd){
+    
+    SSL_CTX_free(ctx);         /* release context */
+    SSL_free(ssl);         /* release SSL state */
+    
     printf("Client stopped\n");
     memset(&clients[fd-4], 0, sizeof(struct sockaddr_in));
     close(fd);
